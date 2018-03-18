d46811412aef959c11924068038b9f84580257d8
jlylb <jlylb@126.com>
2018-03-18 18:56:59 +0800

    list

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         <link href="/css/app.css" rel="stylesheet">
-
+        <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
         <!-- Scripts -->
         <script>
             window.Laravel = <?php
@@ -25,14 +25,92 @@ echo json_encode([
     <body>
         <div id="app">
             <el-container>        
-                <el-header height='60px'>header</el-header>
+                <el-header height='60px'>
+                <el-row>
+<el-col :span="4">
+    33333333333
+</el-col>
+
+  <el-col :span="18">
+      <el-menu  class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="1">处理中心</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">我的工作台</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3">消息中心</el-menu-item>
+        </el-menu>      
+    </el-col>
+    <el-col :span="2">
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item>双皮奶</el-dropdown-item>
+        <el-dropdown-item>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </el-col>
+</el-row>
+
+</el-header>
                 <el-container>
-                    <el-aside width="200px">Aside</el-aside>
+                    <el-aside width="200px">
+                    <el-menu
+                        default-active="1-1"
+                        class="el-menu-vertical-demo">
+                        <el-submenu index="1">
+                            <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                            </template>
+                            <el-menu-item-group>
+                            <template slot="title">分组一</template>
+                            <el-menu-item index="1-1">选项1</el-menu-item>
+                            <el-menu-item index="1-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                            <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                            <template slot="title">选项4</template>
+                            <el-menu-item index="1-4-1">选项1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-document"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                        <el-menu-item index="4">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">导航四</span>
+                        </el-menu-item>
+                        </el-menu>
+                    </el-aside>
                     <el-container>
                         <el-main>
+                            
                             @yield('content')
                         </el-main>
-                        <el-footer><el-button>Hello</el-button></el-footer>
+                        <el-footer>
+
+                        </el-footer>
                     </el-container>
                 </el-container>
             </el-container>
@@ -48,5 +126,7 @@ echo json_encode([
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
+
+        <!-- <script src="https://unpkg.com/element-ui/lib/index.js"></script> -->
     </body>
 </html>
